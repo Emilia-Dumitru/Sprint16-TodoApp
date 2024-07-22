12ca223109ff49bdbfc96a64dff26a623e2cb7f8
Emilia-Dumitru <dumitruemilia41@gmail.com>
2024-07-22 21:16:35 +0300

    am finalizat tema

--- homework/4.3. todo-app/src/components/card/Card.jsx
+++ homework/4.3. todo-app/src/components/card/Card.jsx
@@ -1,7 +1,11 @@
 import "./Card.css";
 
 const Card = (props) => {
-  return <div className="card">{props.children}</div>;
+  return (
+    <div className="card">
+      {props.children}
+    </div>
+  )
 };
 
 export default Card;
--- homework/4.3. todo-app/src/components/todo-item/TodoItem.jsx
+++ homework/4.3. todo-app/src/components/todo-item/TodoItem.jsx
@@ -1,35 +1,79 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./TodoItem.css";
 import Checkbox from "../checkbox/CheckBox";
 
-const TodoItem = (props) => {
+const TodoItem = ({ id, completed, title, description, onToggleCompleted, onDelete, onUpdate }) => {
+  const [isChecked, setIsChecked] = useState(completed);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(title);
+  const [editedDescription, setEditedDescription] = useState(description);
+
+  useEffect(() => {
+    setIsChecked(completed);
+  }, [completed]);
+
   const handleCheckboxChange = (value) => {
-    console.log(value);
+    setIsChecked(value);
+    onToggleCompleted(id, value);
+  };
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleSaveClick = () => {
+    onUpdate(id, editedTitle, editedDescription);
+    setIsEditing(false);
+  };
+
+  const handleCancelClick = () => {
+    setEditedTitle(title);
+    setEditedDescription(description);
+    setIsEditing(false);
   };
 
   return (
-    <div className={`todo-item ${props.completed && "todo-completed"}`}>
+    <div className={`todo-item ${isChecked ? "todo-completed" : ""}`}>
       <div className="todo-item-header">
         <div className="title-area">
           <Checkbox
-            checked={!!props.completed}
+            checked={isChecked}
             onChange={handleCheckboxChange}
           />
-
-          <h4>Assignment 1</h4>
+          {isEditing ? (
+            <input
+              type="text"
+              value={editedTitle}
+              onChange={(e) => setEditedTitle(e.target.value)}
+            />
+          ) : (
+            <h4>{title}</h4>
+          )}
         </div>
         <div>
-          <i className="fa fa-pencil" aria-hidden="true"></i>
-          <i className="fa fa-trash" aria-hidden="true"></i>
+          {isEditing ? (
+            <>
+              <button onClick={handleSaveClick}>Save</button>
+              <button onClick={handleCancelClick}>Cancel</button>
+            </>
+          ) : (
+            <>
+              <i className="fa fa-pencil" aria-hidden="true" onClick={handleEditClick}></i>
+              <i className="fa fa-trash" aria-hidden="true" onClick={() => onDelete(id)}></i>
+            </>
+          )}
         </div>
       </div>
 
       <div className="separator"></div>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. At id illo
-        repellendus non maiores in pariatur aliquam iure fugit amet!
-      </p>
+      {isEditing ? (
+        <textarea
+          value={editedDescription}
+          onChange={(e) => setEditedDescription(e.target.value)}
+        />
+      ) : (
+        <p>{description}</p>
+      )}
     </div>
   );
 };
